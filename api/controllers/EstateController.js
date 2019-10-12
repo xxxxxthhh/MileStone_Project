3988407e789d08707a2b5181056ec33fe554c3d1
19413149 <19413149@cs12191.comp.hkbu.edu.hk>
2019-10-12 21:05:33 +0800

    save for the first time

---
+++ api/controllers/EstateController.js
@@ -0,0 +1,12 @@
+/**
+ * EstateController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
